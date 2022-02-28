0fb9064f409055e2472d530625e254ffcd7f98ca
fabienmacip <fabien.macip@gmail.com>
2022-02-28 15:36:22 +0100

    Avec APP_KEYS

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['DATABASE_URL','postgres://pkhfkugjsijrcz:1ce2bcbf451181d3011758f1aa3647ec8d07d3615675a7dec49095f686e4c748@ec2-3-92-119-83.compute-1.amazonaws.com:5432/d8bv7hn8ni2vnc'],
+    /* keys: env.array('APP_KEYS'), */
   },
 });
